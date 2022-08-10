3a4ea98d91f4e1dd8a81a8716fab21dbed4094db
Nicolas <nicolusmaluleke@gmail.com>
2022-08-11 00:02:50 +0200

    moved build file to project root folder.

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
 dotnet restore -r osx-x64
+# dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:CFBundleDisplayName=Clippy -property:Configuration=Release -p:UseAppHost=true
 dotnet msbuild -t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:CFBundleDisplayName=Clippy -property:Configuration=Release -p:UseAppHost=true
